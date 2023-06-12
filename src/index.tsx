c110f485ffd5a097e0f36e06a409abf594d1107c
Eugene Zimichev <undefz@gmail.com>
2023-06-12 23:19:37 +0300

    support for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
